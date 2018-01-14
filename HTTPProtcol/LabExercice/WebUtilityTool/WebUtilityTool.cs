3891c66c69223eece7620117904942935951265a
FromiMac <niki.spasow@gmail.com>
2018-01-14 22:44:23 +0200

    HTTP Protocol exercises

---
+++ HTTPProtcol/LabExercice/WebUtilityTool/WebUtilityTool.cs
@@ -0,0 +1,28 @@
+﻿namespace WebUtilityTool
+{
+    using System;
+    using System.Net;
+    using System.Net.Http.Headers;
+
+    public class WebUtilityTool
+    {
+        public static void Main()
+        {
+            var url = "https://softuni.bg/trainings/1736/c-sharp-web-development-basics-september-2017#lesson-6465";
+            var decodedUrl = WebUtility.UrlDecode(url);
+
+            //Console.WriteLine(decodedUrl);
+
+            var parsedUrl = new Uri(url);
+
+            Console.WriteLine(parsedUrl.Scheme);
+            Console.WriteLine(parsedUrl.Host);
+            Console.WriteLine(parsedUrl.Port);
+            Console.WriteLine(parsedUrl.AbsolutePath);
+            Console.WriteLine(parsedUrl.Query);
+            Console.WriteLine(parsedUrl.Fragment);
+
+           
+        }
+    }
+}
